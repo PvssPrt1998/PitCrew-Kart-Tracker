443b3967b28d6e4e291efdc0fe7fe4a1261549a6
WebShell1998 <43918790+WebShell1998@users.noreply.github.com>
2024-07-19 17:27:29 +0500

    Initial Commit

---
+++ PitCrew Kart Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PitCrew Kart Tracker
+//
+//  Created by Николай Щербаков on 19.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PitCrew Kart Tracker/PitCrew_Kart_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitCrew_Kart_TrackerApp.swift
+//  PitCrew Kart Tracker
+//
+//  Created by Николай Щербаков on 19.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct PitCrew_Kart_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
